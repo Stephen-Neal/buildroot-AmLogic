30d0c684d654b45c9999c3109b013a7abd6c88b7
stephen <stephen@stephen-i7-Ubuntu.(none)>
2013-01-21 23:50:25 +0000

    Added new defconfig for my STV502 DVB-S2 box called amlogic_f16ref_stvdvb-xbmc_defconfig This points to a new kernel config

--- package/thirdparty/xbmc/xbmc.mk
+++ package/thirdparty/xbmc/xbmc.mk
@@ -6,7 +6,7 @@
 
 XBMC_VERSION = 0987bea35050a3f5d352c3205d75b7173a7d126d
 XBMC_SITE_METHOD = git
-XBMC_SITE = git://github.com/Pivosgroup/xbmc.git
+XBMC_SITE = git://github.com/Stephen-Neal/xbmc.git
 XBMC_INSTALL_STAGING = YES
 XBMC_INSTALL_TARGET = YES
 
